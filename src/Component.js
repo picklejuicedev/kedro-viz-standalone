24f54fc6401c6f621b24aba63ef434d9aa223d74
Sean Westgate <sean@pickle-juice.co.uk>
2022-11-22 12:18:34 +0000

    experiments to get json to load asynchronously not working, statically linked using require.

--- src/Component.js
+++ src/Component.js
@@ -1,19 +1,37 @@
 import KedroViz from "@quantumblack/kedro-viz";
-// import { spaceflights } from "./spaceflights";
+import { spaceflights } from "./spaceflights";
 
+var jsondata = require('./pipeline.json'); //for now, but gets compiled into package 
 
-var json = require('./pipeline.json');
-
+//test async loader
+async function getJsonDataAsync(){
+  console.log("getting Json");
+  var jsondata;
+  return await fetch('/space.json')
+    .then(response => response.json())
+    .then(json => {
+      console.log("data returned");
+      console.log(json);
+      jsondata = json;
+    });
+}
 
 function Component() {
+  console.log("Loading component.")
+
+  //var r = getJsonDataAsync(); //will load data
+  //console.log("r: ", r); // but can't await it as Component can't be async
+  
+  console.log("returning component.")
+
   return (
     <div style={{ height: `100vh`, width: `100%` }}>
       <KedroViz
-        data={json}
+        data={jsondata}
         display={{
           expandAllPipelines: false,
-          globalToolbar: false,
-          miniMap: false,
+          globalToolbar: true,
+          miniMap: true,
           sidebar: true,
         }}
         theme="dark"
